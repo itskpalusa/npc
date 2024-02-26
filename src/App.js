104850b04322b98dc3e8808f0d889a3baf3b3b1b
Karthik Palusa <karthik.palusa@gmail.com>
2024-02-25 21:25:25 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SpotifyPlayer from "./components/SpotifyPlayer";
+
+const App = () => {
+	const [accessToken, setAccessToken] = useState("");
+
+	useEffect(() => {
+		const handleCallback = async () => {
+			const code = new URLSearchParams(window.location.search).get("code");
+			if (code) {
+				try {
+					const params = new URLSearchParams();
+					params.append("grant_type", "authorization_code");
+					params.append("code", code);
+					params.append("redirect_uri", "http://localhost:3000/callback");
+					params.append("client_id", "100c1e60363c4829906eff1efa932728");
+					params.append("client_secret", "a06a51e0f9544e7f916751cf3aed9ab4");
+
+					const response = await axios.post(
+						"https://accounts.spotify.com/api/token",
+						params,
+						{
+							headers: {
+								"Content-Type": "application/x-www-form-urlencoded",
+							},
+						},
+					);
+
+					const {access_token} = response.data;
+					setAccessToken(access_token);
+					localStorage.setItem("accessToken", access_token);
+				} catch (error) {
+					console.error(
+						"Error exchanging code for access token:",
+						error.response.data,
+					);
+				}
+			}
+		};
+
+		handleCallback();
+	}, []);
+
+	const handleLogin = () => {
+		const clientId = "100c1e60363c4829906eff1efa932728";
+		const redirectUri = encodeURIComponent("http://localhost:3000/callback");
+		const scopes = encodeURIComponent(
+			"user-read-private user-read-email user-read-currently-playing",
+		);
+
+		const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
+		window.location.href = authUrl;
+	};
+
+	return (
+		<div className="container center">
+			<h1>Spotify Currently Playing App</h1>
+			{accessToken ? (
+				<SpotifyPlayer accessToken={accessToken} />
+			) : (
+				<div className="btn btn-primary" onClick={handleLogin}>
+					Log in with Spotify
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
