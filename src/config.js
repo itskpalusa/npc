e1d5ef9dfceb74390dcaa5f1ca6d75133a1f026d
Karthik Palusa <karthik.palusa@gmail.com>
2024-02-25 21:20:15 -0800

    Clean Up

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const clientId = "100c1e60363c4829906eff1efa932728";
+export const clientSecret = "a06a51e0f9544e7f916751cf3aed9ab4";
+export const redirectUri = "http://localhost:3000/callback";
