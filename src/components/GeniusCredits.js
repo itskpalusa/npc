f74d7a11820b2a41de61b65143ba02bf244d6467
Karthik Palusa <karthik.palusa@gmail.com>
2024-03-02 15:22:14 -0800

    Loading functionality and animation

--- src/components/GeniusCredits.js
+++ src/components/GeniusCredits.js
@@ -7,159 +7,176 @@ const GeniusCredits = ({currentSong}) => {
 	const [producers, setProducers] = useState("");
 	const [writers, setWriters] = useState("");
 	const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		if (songName && artistName) {
-			searchSong();
-		}
-	}, [songName, artistName]);
+		useEffect(() => {
+			if (songName && artistName) {
+				searchSong();
+			}
+		}, [songName, artistName]);
 
-	const searchSong = async () => {
-		try {
-			const response = await axios.get(
-				`https://api.genius.com/search?q=${songName} ${artistName}&access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
-			);
+		const searchSong = async () => {
+			setLoading(true); // Set loading to true when search is initiated
 
-			const songData = await axios.get(
-				`https://api.genius.com${response.data.response.hits[0].result.api_path}?access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
-			);
+			try {
+				const response = await axios.get(
+					`https://api.genius.com/search?q=${songName} ${artistName}&access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
+				);
 
-			const parsedSongData = songData.data.response.song;
-			const producers = Array.isArray(parsedSongData.producer_artists)
-				? parsedSongData.producer_artists.map((producer) => producer.name)
-				: [];
-			const writers = Array.isArray(parsedSongData.writer_artists)
-				? parsedSongData.writer_artists.map((writer) => writer.name)
-				: [];
+				const songData = await axios.get(
+					`https://api.genius.com${response.data.response.hits[0].result.api_path}?access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
+				);
 
-			setProducers(producers);
-			setWriters(writers);
-			setError(null);
-		} catch (error) {
-			setError("Failed to fetch song data. Please try again.");
-		}
-	};
+				const parsedSongData = songData.data.response.song;
+				const producers = Array.isArray(parsedSongData.producer_artists)
+					? parsedSongData.producer_artists.map((producer) => producer.name)
+					: [];
+				const writers = Array.isArray(parsedSongData.writer_artists)
+					? parsedSongData.writer_artists.map((writer) => writer.name)
+					: [];
 
-	const getCurrentlyPlayingSongDetails = () => {
-		let updatedSongName = "";
-		let updatedArtistName = "";
+				setProducers(producers);
+				setWriters(writers);
+				setError(null);
+			} catch (error) {
+				setError("Failed to fetch song data. Please try again.");
+			} finally {
+				setLoading(false); // Set loading to false after search is completed (whether successful or failed)
+			}
+		};
 
-		if (currentSong.name.includes("(")) {
-			updatedSongName = currentSong.name.split("(")[0];
-			updatedArtistName = currentSong.artists[0].name;
-		} else if (currentSong.name.includes("-")) {
-			updatedSongName = currentSong.name.split("-")[0];
-			updatedArtistName = currentSong.artists[0].name;
-		} else {
-			updatedSongName = currentSong.name;
-			updatedArtistName = currentSong.artists
-				.map((artist) => artist.name)
-				.join(", ");
-		}
+		const getCurrentlyPlayingSongDetails = () => {
+			let updatedSongName = "";
+			let updatedArtistName = "";
 
-		setSongName(updatedSongName);
-		setArtistName(updatedArtistName);
-	};
+			if (currentSong.name.includes("(")) {
+				updatedSongName = currentSong.name.split("(")[0];
+				updatedArtistName = currentSong.artists[0].name;
+			} else if (currentSong.name.includes("-")) {
+				updatedSongName = currentSong.name.split("-")[0];
+				updatedArtistName = currentSong.artists[0].name;
+			} else {
+				updatedSongName = currentSong.name;
+				updatedArtistName = currentSong.artists
+					.map((artist) => artist.name)
+					.join(", ");
+			}
 
-	return (
-		<div className="container">
-			<div>
-				<button
-					className="btn btn-secondary"
-					onClick={getCurrentlyPlayingSongDetails}
-				>
-					Get Credits of Current Song
-				</button>
-			</div>
-			<br />
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-					marginBottom: "3mm",
-				}}
-			>
-				{" "}
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-					}}
-				>
-					<div className="form-row align-items-center">
-						<div className="col">
-							<input
-								id="songNameField"
-								className="form-rowform-group"
-								placeholder="Song Name"
-								value={songName}
-								onChange={(e) => setSongName(e.target.value)}
-							/>
-						</div>
-						<div className="col">
-							<input
-								id="songArtistName"
-								className="form-rowform-group"
-								type="text"
-								placeholder="Artist Name"
-								value={artistName}
-								onChange={(e) => setArtistName(e.target.value)}
-							/>
-						</div>
-						<div className="col">
-							<button className="btn btn-primary" onClick={searchSong}>
-								Search
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<div className="row" style={{paddingBottom: "5px"}}>
-					<div className="col">
-						<div className="card" style={{width: "18rem"}}>
-							{error && <p>{error}</p>}
-							{producers.length > 0 && (
-								<div>
-									<div className="card-header">
-										<h6 className="card-text text-center">Producers</h6>
-									</div>{" "}
-									<ul style={{listStyle: "none", paddingLeft: "0"}}>
-										{producers.map((producer, index) => (
-											<li key={index}>{producer}</li>
-										))}
-									</ul>
-								</div>
-							)}
-						</div>
-					</div>
+			setSongName(updatedSongName);
+			setArtistName(updatedArtistName);
+		};
 
-					<div className="col">
-						<div className="card" style={{width: "18rem"}}>
-							{writers.length > 0 && (
-								<div>
-									<div className="card-header">
-										<h6 className="card-text text-center">Writers</h6>
-									</div>{" "}
-									<ul style={{listStyle: "none", paddingLeft: "0"}}>
-										{writers.map((writer, index) => (
-											<li key={index}>{writer}</li>
-										))}
-									</ul>
-								</div>
-							)}
-						</div>
-					</div>
+		return (
+			<div className="container">
+				<div>
+					<button
+						className="btn btn-secondary"
+						onClick={getCurrentlyPlayingSongDetails}
+					>
+						Get Credits of Current Song
+					</button>
 				</div>
+				<br />
+				{loading ? (
+					<div
+						className="spinner-border"
+						style={{width: "3rem", height: "3rem"}}
+						role="status"
+					>
+						<span className="sr-only">Loading...</span>
+					</div>
+				) : (
+					<div>
+						<div
+							style={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								marginBottom: "3mm",
+							}}
+						>
+							{" "}
+							<form
+								onSubmit={(e) => {
+									e.preventDefault();
+								}}
+							>
+								<div className="form-row align-items-center">
+									<div className="col">
+										<input
+											id="songNameField"
+											className="form-rowform-group"
+											placeholder="Song Name"
+											value={songName}
+											onChange={(e) => setSongName(e.target.value)}
+										/>
+									</div>
+									<div className="col">
+										<input
+											id="songArtistName"
+											className="form-rowform-group"
+											type="text"
+											placeholder="Artist Name"
+											value={artistName}
+											onChange={(e) => setArtistName(e.target.value)}
+										/>
+									</div>
+									<div className="col">
+										<button className="btn btn-primary" onClick={searchSong}>
+											Search
+										</button>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div
+							style={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<div className="row" style={{paddingBottom: "5px"}}>
+								<div className="col">
+									<div className="card" style={{width: "18rem"}}>
+										{error && <p>{error}</p>}
+										{producers.length > 0 && (
+											<div>
+												<div className="card-header">
+													<h6 className="card-text text-center">Producers</h6>
+												</div>{" "}
+												<ul style={{listStyle: "none", paddingLeft: "0"}}>
+													{producers.map((producer, index) => (
+														<li key={index}>{producer}</li>
+													))}
+												</ul>
+											</div>
+										)}
+									</div>
+								</div>
+
+								<div className="col">
+									<div className="card" style={{width: "18rem"}}>
+										{writers.length > 0 && (
+											<div>
+												<div className="card-header">
+													<h6 className="card-text text-center">Writers</h6>
+												</div>{" "}
+												<ul style={{listStyle: "none", paddingLeft: "0"}}>
+													{writers.map((writer, index) => (
+														<li key={index}>{writer}</li>
+													))}
+												</ul>
+											</div>
+										)}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
-		</div>
-	);
+		);
 };
 
 export default GeniusCredits;
