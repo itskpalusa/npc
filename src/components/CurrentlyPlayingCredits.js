f27fe1408d258b78c75dccc81ab1717ad124b215
Karthik Palusa <karthik.palusa@gmail.com>
2024-03-02 16:16:45 -0800

    correct loading position

--- src/components/CurrentlyPlayingCredits.js
+++ src/components/CurrentlyPlayingCredits.js
@@ -77,17 +77,22 @@ const CurrentlyPlayingCredits = ({currentSong}) => {
 			<br />
 			{loading ? (
 				<div
-					className="spinner-border"
 					style={{
-						width: "3rem",
-						height: "3rem",
 						display: "flex",
 						alignItems: "center",
 						justifyContent: "center",
 					}}
-					role="status"
 				>
-					<span className="sr-only">Loading...</span>
+					<div
+						className="spinner-border"
+						style={{
+							width: "3rem",
+							height: "3rem",
+						}}
+						role="status"
+					>
+						<span className="sr-only">Loading...</span>
+					</div>
 				</div>
 			) : (
 				<div>
