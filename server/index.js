b2173822f4fcac0d0030635668c85cc1791c3599
Karthik Palusa <karthik.palusa@gmail.com>
2024-03-11 19:58:36 -0700

    Revert "include callback"
    This reverts commit ea3b747b723e22465c1798304153eabf4c268ae7.

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ var spotify_redirect_uri = "http://localhost:3000/auth/callback";
 const spotify_redirect_uri =
 	process.env.NODE_ENV !== "production"
 		? "http://localhost:3000/auth/callback"
-		: "https://main--spotify-npc.netlify.app/auth/callback"; 
+		: "https://main--spotify-npc.netlify.app/"; 
 
 var generateRandomString = function (length) {
 	var text = "";
