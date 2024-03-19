2b4e2184cbdbdb30d94428b237ba23c68951de42
Karthik Palusa <karthik.palusa@gmail.com>
2024-03-18 19:55:25 -0700

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+// Navbar.js
+import React from "react";
+
+const Navbar = ({accessToken, handleLogout}) => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container justify-content-between">
+				<a className="navbar-brand" href="#">
+					NPC
+				</a>
+				<div className="text-end">
+					{accessToken && (
+						<button className="btn btn-info me-2" onClick={handleLogout}>
+							Logout
+						</button>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
