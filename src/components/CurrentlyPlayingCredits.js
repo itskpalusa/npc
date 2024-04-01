bdcd28d466f28ad25c1806fc7c4751b2507524e6
Karthik Palusa <karthik.palusa@gmail.com>
2024-03-31 20:24:02 -0700

    cleanup

--- src/components/CurrentlyPlayingCredits.js
+++ src/components/CurrentlyPlayingCredits.js
@@ -8,48 +8,48 @@ const CurrentlyPlayingCredits = ({currentSong}) => {
 	const [writers, setWriters] = useState("");
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
+const access_tk = "INSERT GENIUS ACCESS KEY";
+useEffect(() => {
+	if (currentSong) {
+		getCurrentlyPlayingSongDetails();
+	}
+}, [currentSong]);
 
-	useEffect(() => {
-		if (currentSong) {
-			getCurrentlyPlayingSongDetails();
-		}
-	}, [currentSong]);
+useEffect(() => {
+	if (songName && artistName) {
+		searchSong();
+	}
+}, [songName, artistName]);
 
-	useEffect(() => {
-		if (songName && artistName) {
-			searchSong();
-		}
-	}, [songName, artistName]);
+const searchSong = async () => {
+	setLoading(true); // Set loading to true when search is initiated
 
-	const searchSong = async () => {
-		setLoading(true); // Set loading to true when search is initiated
+	try {
+		const response = await axios.get(
+			`https://api.genius.com/search?q=${songName} ${artistName}&access_token=${access_tk}`,
+		);
 
-		try {
-			const response = await axios.get(
-				`https://api.genius.com/search?q=${songName} ${artistName}&access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
-			);
+		const songData = await axios.get(
+			`https://api.genius.com${response.data.response.hits[0].result.api_path}?access_token=${access_tk}`,
+		);
 
-			const songData = await axios.get(
-				`https://api.genius.com${response.data.response.hits[0].result.api_path}?access_token=HSpJDGiFMsHxm9StKheXxSTCpuNSWr1dtKqaj69rS5dlBfH2Wgz3v2xUAL8X6vzY`,
-			);
+		const parsedSongData = songData.data.response.song;
+		const producers = Array.isArray(parsedSongData.producer_artists)
+			? parsedSongData.producer_artists.map((producer) => producer.name)
+			: [];
+		const writers = Array.isArray(parsedSongData.writer_artists)
+			? parsedSongData.writer_artists.map((writer) => writer.name)
+			: [];
 
-			const parsedSongData = songData.data.response.song;
-			const producers = Array.isArray(parsedSongData.producer_artists)
-				? parsedSongData.producer_artists.map((producer) => producer.name)
-				: [];
-			const writers = Array.isArray(parsedSongData.writer_artists)
-				? parsedSongData.writer_artists.map((writer) => writer.name)
-				: [];
-
-			setProducers(producers);
-			setWriters(writers);
-			setError(null);
-		} catch (error) {
-			setError("Failed to fetch song data. Please try again.");
-		} finally {
-			setLoading(false); // Set loading to false after search is completed (whether successful or failed)
-		}
-	};
+		setProducers(producers);
+		setWriters(writers);
+		setError(null);
+	} catch (error) {
+		setError("Failed to fetch song data. Please try again.");
+	} finally {
+		setLoading(false); // Set loading to false after search is completed (whether successful or failed)
+	}
+};
 
 	const getCurrentlyPlayingSongDetails = () => {
 		let updatedSongName = "";
